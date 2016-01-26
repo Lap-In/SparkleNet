bb3b973a40b2ba35a47e435c1ebef4b21972dce7
Kevin POULAIN <kevin1.poulain@etudiant.univ-lille1.fr>
2016-01-26 11:16:23 +0100

    Premier client-serveur qui fonctionne
    Validée par le Prof :D
    
    Ajoute les sources permettant d'ouvrir un serveur:
    	* socket.h & socket.c : Module qui permet d'ouvrir un serveur;
    	* main.c : Active le serveur et attend des réponses du client.
    
    Socket.c =
    	On crée une socket pour IPv4 et en protocole TCP. Puis on associe la
    socket avec la commande bind en utilisant toutes les adresses IP de toutes les
    interfaces et avec le port spécifié. Ensuite, on active l'écoute de connexion,
    au nombre de 10 maximum.
    
    Quand on exécute le main, on crée un serveur avec le port 8080, puis on
    accepte les sockets d'un client.
    Quand le serveur reçoit une socket client avec la commande 'nc localhost 8080',
    il attend une seconde grâce à sleep puis affiche un message de bienvenue(passage
    de la fanfic Fallout:Equestria). Puis à chaque nouveau message envoyé par le
    client, il le réaffiche. Enfin, quand le client se déconnecte, close sur les
    sockets.

--- webserver/main.c
+++ webserver/main.c
@@ -1,13 +1,46 @@
 #include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
 #include <string.h>
+#include <sys/types.h>
+#include <sys/socket.h>
+
+#include "socket.h"
 
 int main(int argc, char ** argv)
 {
 	/* Arnold Robbins in the LJ of February ’95 , describing RCS */
-	if (argc>1 && strcmp(argv[1], "- advice") == 0) {
+	if (argc>1 && strcmp(argv[1], "-advice") == 0) {
 		printf("Don ’t Panic !\n");
 		return 42;
 	}
-	printf("Need an advice ?\n");
+	
+	int socket_serveur = creer_serveur(8080);
+
+	int socket_client;
+
+	socket_client = accept(socket_serveur, NULL, NULL );
+		if (socket_client == -1)
+		{
+			perror ("accept");
+			return -1;
+		}
+
+		sleep(1);
+
+/* On peut maintenant dialoguer avec le client */
+		const char *message_bienvenue = "What is a PipBuck?  A PipBuck is a device, worn on a foreleg just above the hoof, issued to every pony in a Stable when they become old enough to start work.  A blending of unicorn pony magic and science, your PipBuck will keep a constant measure of your health and even help administer healing poultices and other medicine, track and organize everything in your saddlepacks, assist in repairs, and keep all manner of notes and maps available at a hooftap.  Plus, it allows you to listen to the Stable broadcast whenever you would like as it can tune into and decrypt just about any radio frequency.  And that’s not all. A pony’s PipBuck generates an E.F.S. (Eyes-Forward Sparkle) that will indicate direction and help gauge whether the ponies or creatures around you are hostile.  And, perhaps most impressively, a PipBuck can magically aid you in a fight for brief periods of time through use of the S.A.T.S. (Stable-Tec Arcane Targeting Spell). Oh, and a feature not to be forgotten: it can keep track of the location of tagged objects or people, including the wearers of other PipBucks.  So if a pony somehow got lost -- don’t ask me how you could get lost in a Stable, but it does happen on occasion -- then anypony who knew the lost pony’s tag could find them instantly.\nIt can even be made to glow like a lamp.\n";
+		write(socket_client, message_bienvenue, strlen(message_bienvenue));
+
+	while(1) {
+
+		char *buffer = malloc(300*sizeof(char));
+		read(socket_client, buffer, 300*sizeof(char));
+		write(socket_client, buffer, strlen(buffer));
+	}
+
+	close(socket_client);
+	close(socket_serveur);
+
 	return 0;
 }
---
+++ webserver/socket.c
@@ -0,0 +1,49 @@
+#include <stdio.h>
+#include <arpa/inet.h>
+#include <sys/types.h>
+#include <sys/socket.h>
+
+#include "socket.h"
+
+/** Crée une socket serveur qui écoute sur toute les interfaces IPv4
+de la machine sur le port passé en paramètre . La socket retournée
+doit pouvoir être utilisée directement par un appel à accept .
+La fonction retourne -1 en cas d ’ erreur ou le descripteur de la
+socket créée . */
+int creer_serveur(int port)
+{
+	/** Creation de la socket */
+
+	int socket_serveur;
+
+	socket_serveur = socket(AF_INET, SOCK_STREAM, 0);
+	if (socket_serveur == -1)
+	{
+		perror ( " socket_serveur " );
+		return -1;
+	}
+	/* Utilisation de la socket serveur */
+
+	/** Configuration de la socket */
+
+	struct sockaddr_in saddr;
+
+	saddr.sin_family = AF_INET; /* Socket ipv4 */
+	saddr.sin_port = htons(port); /* Port d ’ écoute */
+	saddr.sin_addr.s_addr = INADDR_ANY; /* écoute sur toutes les interfaces */
+	
+	if (bind(socket_serveur, (struct sockaddr *) &saddr , sizeof(saddr)) == -1)
+	{
+		perror ("bind socker_serveur");
+		return -1;
+	}
+
+	/** Démarer l'attente de connexions */
+	if (listen(socket_serveur,10) == -1)
+	{
+		perror("listen socket_serveur");
+		return -1;
+	}
+
+	return socket_serveur;
+}
