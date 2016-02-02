b8c56a6ff1693fecf7ae8f544f5aebcfbdb956c4
Pohle Maxime <pohle.maxime@gmail.com>
2016-02-02 11:56:50 +0100

    Ajout de la fonction init_pid_capture Cette fonction utilise la fonction sigaction.

--- webserver/main.c
+++ webserver/main.c
@@ -15,9 +15,9 @@ int traitement_client(int socket_client){
   sleep(1);
 
   /* Affichage du message de bienvenue */
-  const char *message_bienvenue = "What is a PipBuck?  A PipBuck is a device, worn on a foreleg just above the hoof, issued to every pony in a Stable when they become old enough to start work.  A blending of unicorn pony magic and science, your PipBuck will keep a constant measure of your health and even help administer healing poultices and other medicine, track and organize everything in your saddlepacks, assist in repairs, and keep all manner of notes and maps available at a hooftap.  Plus, it allows you to listen to the Stable broadcast whenever you would like as it can tune into and decrypt just about any radio frequency.  And that’s not all. A pony’s PipBuck generates an E.F.S. (Eyes-Forward Sparkle) that will indicate direction and help gauge whether the ponies or creatures around you are hostile.  And, perhaps most impressively, a PipBuck can magically aid you in a fight for brief periods of time through use of the S.A.T.S. (Stable-Tec Arcane Targeting Spell). Oh, and a feature not to be forgotten: it can keep track of the location of tagged objects or people, including the wearers of other PipBucks.  So if a pony somehow got lost -- don’t ask me how you could get lost in a Stable, but it does happen on occasion -- then anypony who knew the lost pony’s tag could find them instantly.\nIt can even be made to glow like a lamp.\n";
+  char *message_bienvenue = "What is a PipBuck?  A PipBuck is a device, worn on a foreleg just above the hoof, issued to every pony in a Stable when they become old enough to start work.  A blending of unicorn pony magic and science, your PipBuck will keep a constant measure of your health and even help administer healing poultices and other medicine, track and organize everything in your saddlepacks, assist in repairs, and keep all manner of notes and maps available at a hooftap.  Plus, it allows you to listen to the Stable broadcast whenever you would like as it can tune into and decrypt just about any radio frequency.  And that’s not all. A pony’s PipBuck generates an E.F.S. (Eyes-Forward Sparkle) that will indicate direction and help gauge whether the ponies or creatures around you are hostile.  And, perhaps most impressively, a PipBuck can magically aid you in a fight for brief periods of time through use of the S.A.T.S. (Stable-Tec Arcane Targeting Spell). Oh, and a feature not to be forgotten: it can keep track of the location of tagged objects or people, including the wearers of other PipBucks.  So if a pony somehow got lost -- don’t ask me how you could get lost in a Stable, but it does happen on occasion -- then anypony who knew the lost pony’s tag could find them instantly.\nIt can even be made to glow like a lamp.\n ";
   write(socket_client, message_bienvenue, strlen(message_bienvenue));
-
+ 
   /* Affichage et lecture des messages envoyées par le client */
   while(1) {
     char *buffer = malloc(300*sizeof(char));
@@ -47,12 +47,14 @@ int main(int argc, char ** argv)
     socket_client = accept(socket_serveur, NULL, NULL);
     /* Ignore le signal SIGPIPE */
     initialiser_signaux();
+    init_pid_capture();
     /* Traitement d'erreur */
-    if (socket_client == -1)
-      {
-	perror ("accept");
-	return -1;
-      }
+    if (socket_client == -1) {
+      perror ("accept");
+      return -1;
+    }else{
+      printf("Client = %d\n", socket_client);
+    }
 
     /* Crée un processus de traitement d'une connexion */
     int pid = fork();
--- webserver/socket.c
+++ webserver/socket.c
@@ -1,60 +1,75 @@
 #include <stdio.h>
 #include <arpa/inet.h>
 #include <sys/types.h>
+#include <sys/wait.h>
 #include <sys/socket.h>
 #include <signal.h>
 
 #include "socket.h"
 
 /** Crée une socket serveur qui écoute sur toute les interfaces IPv4
-de la machine sur le port passé en paramètre . La socket retournée
-doit pouvoir être utilisée directement par un appel à accept .
-La fonction retourne -1 en cas d ’ erreur ou le descripteur de la
-socket créée . */
+    de la machine sur le port passé en paramètre . La socket retournée
+    doit pouvoir être utilisée directement par un appel à accept .
+    La fonction retourne -1 en cas d ’ erreur ou le descripteur de la
+    socket créée . */
 int creer_serveur(int port)
 {
-	/** Creation de la socket */
+  /** Creation de la socket */
 
-	int socket_serveur;
+  int socket_serveur;
 
-	socket_serveur = socket(AF_INET, SOCK_STREAM, 0);
-	if (socket_serveur == -1)
-	{
-		perror ( " socket_serveur " );
-		return -1;
-	}
-	int optval = 1;
-	if (setsockopt(socket_serveur, SOL_SOCKET, SO_REUSEADDR, &optval,
-		       sizeof(int)) == -1)
-	  perror("Can not set SO_REUSEADDR option");
-	/* Utilisation de la socket serveur */
+  socket_serveur = socket(AF_INET, SOCK_STREAM, 0);
+  if (socket_serveur == -1)
+    {
+      perror ("socket_serveur");
+      return -1;
+    }
+  int optval = 1;
+  if (setsockopt(socket_serveur, SOL_SOCKET, SO_REUSEADDR, &optval,
+		 sizeof(int)) == -1)
+    perror("Can not set SO_REUSEADDR option");
+  /* Utilisation de la socket serveur */
 
-	/** Configuration de la socket */
+  /** Configuration de la socket */
 
-	struct sockaddr_in saddr;
+  struct sockaddr_in saddr;
 
-	saddr.sin_family = AF_INET; /* Socket ipv4 */
-	saddr.sin_port = htons(port); /* Port d ’ écoute */
-	saddr.sin_addr.s_addr = INADDR_ANY; /* écoute sur toutes les interfaces */
+  saddr.sin_family = AF_INET; /* Socket ipv4 */
+  saddr.sin_port = htons(port); /* Port d ’ écoute */
+  saddr.sin_addr.s_addr = INADDR_ANY; /* écoute sur toutes les interfaces */
 	
-	if (bind(socket_serveur, (struct sockaddr *) &saddr , sizeof(saddr)) == -1)
-	{
-		perror ("bind socker_serveur");
-		return -1;
-	}
+  if (bind(socket_serveur, (struct sockaddr *) &saddr , sizeof(saddr)) == -1)
+    {
+      perror ("bind socker_serveur");
+      return -1;
+    }
 
-	/** Démarer l'attente de connexions */
-	if (listen(socket_serveur,10) == -1)
-	{
-		perror("listen socket_serveur");
-		return -1;
-	}
+  /** Démarer l'attente de connexions */
+  if (listen(socket_serveur,10) == -1)
+    {
+      perror("listen socket_serveur");
+      return -1;
+    }
 
-	return socket_serveur;
+  return socket_serveur;
 }
+
+void init_pid_capture()
+{
+  struct sigaction sa ;
+  sa.sa_handler = (void *) waitpid;
+  sigemptyset(&sa.sa_mask);
+  sa.sa_flags = SA_RESTART ;
+  if(sigaction(SIGCHLD,&sa, NULL) == -1)
+    {
+      perror ("sigaction(SIGCHLD)" );
+    }
+    }
+
 void initialiser_signaux(void){
   if(signal(SIGPIPE, SIG_IGN) == SIG_ERR)
     {
       perror ("signal");
     }
 }
+
